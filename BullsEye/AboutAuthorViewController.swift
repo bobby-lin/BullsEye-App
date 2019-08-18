8432b3aca25d484bb8a9f3e35c81400853f08a12
bobby-lin <mr.linjiahao@gmail.com>
2019-08-19 00:18:26 +0800

    Add About the Author Screen

---
+++ BullsEye/AboutAuthorViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AboutAuthorViewController.swift
+//  BullsEye
+//
+//  Created by gamma on 19/8/19.
+//  Copyright © 2019 Demo. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+}
